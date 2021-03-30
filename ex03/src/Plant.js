fe84c9475af7e65d30af05a88b29d18c94c7863e
stefan <stefansgit@gmail.com>
2021-03-30 09:39:51 +0200

    fix

--- ex03/src/Plant.js
+++ ex03/src/Plant.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 
 class Plant extends Component {
     render() { 
-        return ( 
-            <h2>This is a Plant Component!</h2>
-         );
+        return <h2>This is a Plant Component!</h2>;
     }
 }
  
