7c682b529cc44391d90b31afeeaf29e02ca69e75
stefan <stefansgit@gmail.com>
2021-03-30 10:14:25 +0200

    fix

--- ex09/src/Flower.js
+++ ex09/src/Flower.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 
 class Flower extends Component {
     render() { 
-        return ( 
-        <h2>This is a {this.props.color} Flower!</h2>
-         );
+        return <h2>This is a {this.props.color} Flower!</h2>;
     }
 }
  
