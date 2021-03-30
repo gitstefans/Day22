66e58c630fc8f43fc41275e0d749d3d94c43b591
stefan <stefansgit@gmail.com>
2021-03-30 10:21:58 +0200

    fix

--- ex10/src/Box.js
+++ ex10/src/Box.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 
 function Box(props) {
-    return (
-        <h2>This is a {props.size}px Box!</h2>
-    )
+    // Only change code below this line
+    return <h2>This is a {props.size}px Box!</h2>;
+    // Only change code above this line
 }
 
 export default Box;
